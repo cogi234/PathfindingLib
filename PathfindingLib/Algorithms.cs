03f921d8d451bd08a248ce9f92eadd51d2d464aa
electrix1337 <95308274+electrix1337@users.noreply.github.com>
2023-09-15 14:41:40 -0400

    toi toi

--- PathfindingLib/Algorithms.cs
+++ PathfindingLib/Algorithms.cs
@@ -2,8 +2,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace PathfindingLib
 {
@@ -93,6 +95,46 @@ namespace PathfindingLib
                     {
                         cost_so_far[next.Item1] = cost_so_far[next.Item1]+next.Item2;
                         frontier.Enqueue(next.Item1,next.Item2 + cost_so_far[next.Item1]);
+                        cameFrom[next.Item1] = current;
+                    }
+                }
+            }
+
+            return new List<int>();
+        }
+
+        public static List<int> AStar(Func<int, int, int> funcky, IWeightedGraphRepresentation graph, int startNode, int endNode)
+        {
+            if (startNode == endNode)
+                return new List<int>() { startNode };
+            var cost_so_far = new int[graph.VertexCount];
+            cost_so_far[startNode] = 0;
+            var frontier = new PriorityQueue<int, int>();
+            var cameFrom = new int[graph.VertexCount];
+            Array.Fill(cameFrom, -1);
+
+            frontier.Enqueue(startNode, 0);
+
+            while (frontier.Count > 0)
+            {
+                int current = frontier.Dequeue();
+                IEnumerable<(int neighbour, int cost)> currentNeighbours = graph.GetNeighbours(current);
+
+                for (int i = 0; i < currentNeighbours.Count(); ++i)
+                {
+                    (int neightbour, int cout) next = currentNeighbours.ElementAt(i);
+
+                    if (next.Item1 == endNode)
+                    {
+
+                        cameFrom[next.Item1] = current;
+                        return BuildShortestPath(startNode, endNode, cameFrom);
+                    }
+
+                    if (cameFrom[next.Item1] == -1)
+                    {
+                        cost_so_far[next.Item1] = cost_so_far[next.Item1] + next.Item2;
+                        frontier.Enqueue(next.Item1, next.Item2 + cost_so_far[next.Item1]);
                         cameFrom[next.Item1] = current;
                     }
                 }
